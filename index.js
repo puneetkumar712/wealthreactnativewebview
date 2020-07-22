c640c462f5f7fdd2ecb0e1601ee5989ffcbbe0f3
puneet <puneetkumar712@gmail.com>
2020-07-22 13:46:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
+
+console.log("Lets do it");
+
+console.log("Lets learn the javascript")
+
+console.log("Lets try to do it again");
+
+console.log("Now to do it again and try again");
